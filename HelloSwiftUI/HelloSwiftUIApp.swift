fd2401a6bcb8c43366e9d0da9d98e8630cbf16d2
Mostafa Shuman <mostafa.shuman@mondia.com>
2021-03-05 03:24:25 +0200

    Initial Commit

---
+++ HelloSwiftUI/HelloSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloSwiftUIApp.swift
+//  HelloSwiftUI
+//
+//  Created by Mostafa Shuman on 05/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct HelloSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
