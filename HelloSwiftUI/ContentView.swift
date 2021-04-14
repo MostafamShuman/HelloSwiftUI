fa6638702b6eced65f65691956b347213187eba9
Mostafa Shuman <mostafa.shuman@mondia.com>
2021-04-14 14:15:50 +0200

    implement binding

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -8,14 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+  
+  @State var isPlaying: Bool = false
+  
+  var body: some View {
+    VStack {
+      Text("New Track")
+        .font(.title)
+        .foregroundColor(self.isPlaying ? .red : /*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+      Text("artist")
+        .foregroundColor(.secondary)
+      PlayButton(isPlaying: $isPlaying)
+      
     }
+  }
 }
 
+struct PlayButton: View {
+  @Binding var isPlaying: Bool
+  var body: some View {
+    Button(action: toggleState, label: {
+      Text("Done")
+    })
+    .padding()
+  }
+  
+  func toggleState() {
+    isPlaying.toggle()
+  }
+}
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
